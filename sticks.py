b573dd6d683a6e4d797bec40506bd7972f3f5e8e
NwaSet <samainflorian25@gmail.com>
2026-02-04 09:32:03 +0100

    player

--- sticks.py
+++ sticks.py
@@ -1,28 +1,44 @@
+import random 
+
 class Game :
     None
 
 class Player :
-    def __init__(self, name):
+    """
+    A class represent a player
+
+    Attributs:
+        name (str)  : Player name
+        game (Game) : Player game where he can be playing, he is not obliged to be in a game. 
+    """
+    def __init__(self, name, game=None):
         self.name = name
+        self.game = game
         self.nb_win = 0
         self.nb_loose = 0
     
     @property
     def nb_game(self) :
+        """
+        return : the number of game plaied by the player
+        """
         return self.nb_loose + self.nb_win
     
     def play(self) :
-        pass
+        choice = random.randint(1,3)
+        return choice
 
     def win(self) :
-        pass
+        self.nb_win += 1
 
     def loose(self) :
-        pass
+        self.nb_loose += 1
         
 
 class Human(Player) :
-    None
+    def play(self) :
+        choice = input("how many sticks do you wan't to take (1 - 3): ")
+        return choice
 
 class Ai(Player) :
     None
