616fe8b914c3b91940c743b8bd66feb5e9243131
NwaSet <samainflorian25@gmail.com>
2026-02-06 20:38:22 +0100

    starting implementation interface

--- sticks.py
+++ sticks.py
@@ -1,9 +1,15 @@
-import random 
+import random
+from tkinter import *
+
+"""
+modification :
+    enlever l'init de interface, comme ca on commence par faire le link entre toutes les class
+"""
 
 class Game :
-    def __init__(self, player1, player2, nb=12, displayable =True):
-        self.original_nb = nb
-        self.nb = nb
+    def __init__(self, player1, player2, nb_stick=12, displayable =True):
+        self.original_nb_stick = nb_stick
+        self.nb_stick = nb_stick
         
         self.player1 = player1
         self.player2 = player2
@@ -22,21 +28,21 @@ class Game :
         self.player1, self.player2 = players
          
     def reset(self):
-        self.nb = self.original_nb
+        self.nb_stick = self.original_nb_stick
         self.shuffle()
         
     def display(self):
         if self.displayable:
-            print(f"Allumettes restantes : {self.nb}")
+            print(f"Allumettes restantes : {self.nb_stick}")
             
     def step(self, action):
         if (action < 1 or action > 3):
             return False
 
-        if action > self.nb:
-            action = self.nb
+        if action > self.nb_stick:
+            action = self.nb_stick
 
-        self.nb -= action
+        self.nb_stick -= action
         return True
 
     def play (self):
@@ -57,13 +63,7 @@ class Game :
 
         winner.win()
         loser.loose()
-        
-        print(f"{winner.name} is the winner with {winner.nb_win} ")
-            
-        
-        
-        
-
+                  
 class Player :
     """
     A class represent a player
@@ -92,7 +92,6 @@ class Player :
     def loose(self) :
         self.nb_loose += 1
         
-
 class Human(Player) :
     def play(self) :
         choice = int(input("how many sticks do you wan't to take (1 - 3): "))
@@ -101,9 +100,46 @@ class Human(Player) :
 class Ai(Player) :
     None
 
+class Interface :
+    def __init__(self, controler):
+
+        self.controler = controler
+
+        self.root = Tk()
+        self.root.title("Mikado Game")
+        self.root.geometry("850x450")
+        self.root.resizable(False, False)
+
+        self.canvas = Canvas(self.root, width= 800, height=300)
+        self.canvas.pack()
+
+        self.root.mainloop()
+
+    
+    def set_controler(self, controler) :
+        self.controler = controler
+    
+    def show_stick(self, x_pos) :
+        self.canvas.create_rectangle(x_pos, 100, x_pos+5, 250, fill="brown")
+    
+    def init_stick(self) :
+        nb_stick = self.controler.get_nb_stick()
+        for i in range(nb_stick) :
+            self.show_stick(i*10)
+
+class Controler :
+    def __init__(self, game, interface):
+        self.game = game
+        self.interface = interface
+    
+    def get_nb_stick(self) :
+        return self.game.nb_stick
+
+
 if __name__ == "__main__":
     player1 = Human("yo")
     player2 = Player("flo")
     
+    controler = Controler()
     game = Game(player1,player2)
-    game.play()
+    gui = Interface(controler)
