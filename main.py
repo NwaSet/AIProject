5fe4d024c96d240b4f6c422b91fda555ccf9947e
NwaSet <samainflorian25@gmail.com>
2026-02-10 09:25:51 +0100

    clearing folder

---
+++ main.py
@@ -0,0 +1,104 @@
+import random
+from tkinter import *
+
+class Game :
+    def __init__(self, player1, player2, nb_stick=12, displayable =True):
+        self.original_nb_stick = nb_stick
+        self.nb_stick = nb_stick
+        
+        self.player1 = player1
+        self.player2 = player2
+        
+        self.displayable = displayable
+        
+        self.player1.game = self
+        self.player2.game = self
+        
+        self.shuffle()
+
+        self.controler = None
+        
+       
+    def shuffle(self):
+        players = [self.player1, self.player2]
+        random.shuffle(players)
+        self.player1, self.player2 = players
+         
+    def reset(self):
+        self.nb_stick = self.original_nb_stick
+        self.shuffle()
+        
+    def display(self):
+        if self.displayable:
+            print(f"Allumettes restantes : {self.nb_stick}")
+            
+    def step(self, action):
+        if (action < 1 or action > 3):
+            return False
+
+        if action > self.nb_stick:
+            action = self.nb_stick
+
+        self.nb_stick -= action
+        return True
+
+    def play (self):
+        self.reset
+        
+        current_player = self.player1
+        other_player = self.player2
+        
+        while self.nb>0:
+            self.display()
+            
+            self.step(current_player.play())
+            
+            current_player, other_player = other_player, current_player
+            
+        winner = current_player
+        loser = other_player
+
+        winner.win()
+        loser.loose()
+                  
+class Player :
+    """
+    A class represent a player
+
+    Attributs:
+        name (str)  : Player name
+        game (Game) : Player game where he can be playing, he is not obliged to be in a game. 
+    """
+    def __init__(self, name, game=None):
+        self.name = name
+        self.game = game
+        self.nb_win = 0
+        self.nb_loose = 0
+    
+    @property
+    def nb_game(self: object) -> int:
+        return self.nb_loose + self.nb_win
+    
+    def play(self) :
+        choice = random.randint(1,3)
+        return choice
+
+    def win(self) :
+        self.nb_win += 1
+
+    def loose(self) :
+        self.nb_loose += 1
+        
+class Human(Player) :
+    def play(self) :
+        choice = int(input("how many sticks do you wan't to take (1 - 3): "))
+        return choice
+
+class Ai(Player) :
+    None
+
+if __name__ == "__main__":
+    player1 = Human("yo")
+    player2 = Player("flo")
+    
+    game = Game(player1,player2)
