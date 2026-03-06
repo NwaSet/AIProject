5d9e1f05874ee57f8cae89efe330f9cd0adf4831
NwaSet <samainflorian25@gmail.com>
2026-03-06 13:10:32 +0100

    model

--- games/Cubee/Model.py
+++ games/Cubee/Model.py
@@ -1,20 +1,30 @@
 import random
 
 class GameModel :
-    def __init__(self, grid_size, Player1, Player2):
+    def __init__(self, grid_size, Player1=None, Player2=None):
 
         self.grid_size = grid_size
-        self.grid[self.grid_size] = []
+        self.grid = []
+        self.init_grid()
 
         self.player1 = Player1
         self.player2 = Player2
 
-        self.player1.coord = (self.grid_size-1, self.grid_size-1)
-        self.player2.coord = (0, 0)        
+        #self.player1.coord = (self.grid_size-1, self.grid_size-1)
+        #self.player2.coord = (0, 0)        
 
         self.score = {self.player1.__str__() : 0, self.player2.__str__() : 0}
 
-        self.current_player = self.shuffle()
+        #self.current_player = self.shuffle()
+    
+    def init_grid(self) :
+        for x in range(self.grid_size) :
+            row = []
+            for y in range(self.grid_size) :
+                row.append(0)
+            self.grid.append(row)
+        self.grid[0][0] = 2
+        self.grid[self.grid_size-1][self.grid_size-1] = 1
     
     def shuffle(self) :
         return random.choice(self.player1, self.player2)
@@ -23,8 +33,15 @@ class GameModel :
         self.current_player = self.player1 if self.current_player == self.player2 else self.player2
     
     def reset(self) :
+
         self.grid[self.grid_size] = []
+
+        self.player1.coord = (self.grid_size-1, self.grid_size-1)
+        self.player2.coord = (0, 0) 
+
         self.current_player = self.shuffle()
-        self.score = (0, 0)
-        
-        
+
+        self.score = {self.player1.__str__() : 0, self.player2.__str__() : 0}
+
+test = GameModel(3)
+print(test.grid)
