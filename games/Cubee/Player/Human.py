726656852d515afbf3330921f40735570cf939a2
Nhanyo <yohannducourtieux@gmail.com>
2026-03-13 12:27:11 +0100

    v

--- games/Cubee/Player/Human.py
+++ games/Cubee/Player/Human.py
@@ -1,12 +1,8 @@
 from .Player import *
-import keybord
+
 
 class Human(Player):
-    def __init__(self,name,color, game = None):
-        super.__init__(name,game)
-        
-        self.color = color
+    def __init__(self, name, color, game=None):
+        super.__init__(name, game)
 
-    def play(self):
-        return keybord.read_key()
-        
+        self.color = color
