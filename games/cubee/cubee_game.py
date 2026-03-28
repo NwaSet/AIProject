85b833fd0bd0a1892f3a53de078aeadc660374f7
NwaSet <samainflorian25@gmail.com>
2026-03-28 23:37:05 +0100

    clean code cubee game

--- games/cubee/cubee_game.py
+++ games/cubee/cubee_game.py
@@ -12,14 +12,7 @@ def start_cubee_game():
 
     flo = Human(1, "flo", "red")
     yo = Human(2, "yo", "blue")
-
     ctrl = gameControler()
-
     game = GameModel(5, Player1=flo, Player2=yo, controler=ctrl)
-    # print(game.grid)
-    # ctrl.handle_human_move((1,0))
-    # ctrl.handle_human_move((1,0))
-    # print(game.grid)
-
     view = View(ctrl)
     ctrl.start_game()
