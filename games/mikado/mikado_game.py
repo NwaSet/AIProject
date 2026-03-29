0c8861ec5e62a1f8c5693326c0606870c44631e9
NwaSet <samainflorian25@gmail.com>
2026-03-29 17:14:37 +0200

    clean code

--- games/mikado/mikado_game.py
+++ games/mikado/mikado_game.py
@@ -35,25 +35,3 @@ def start_mikado_Game() :
     game_view = GameView(game_controler)
     game = GameModel(player1,Bob, game_controler)
     game_controler.start_game()
-    # Print a comparison between the @ais
-    names = f"{'':4}"
-    stats1 = f"{'':4}"
-    stats2 = f"{'':4}"
-
-    for ai in ais :
-        names += f"{ai.name:^15}"
-        stats1 += f"{str(ai.nb_win)+'/'+str(ai.nb_game):^15}"
-        stats2 += f"{f'{ai.nb_win/ai.nb_game*100:4.4}'+'%':^15}"
-
-    print(names)
-    print(stats1)
-    print(stats2)
-    print(f"{'-'*4}{'-'*len(ais)*15}")
-
-    all_v_dict = {key : [ai.v_fuction.get(key,0.0) for ai in ais] for key in ais[0].v_fuction.keys()}
-    sorted_v = lambda v_dict : sorted(filter(lambda x : type(x[0])==int ,v_dict.items()))
-    for state, values in sorted_v(all_v_dict):
-        print(f"{state:2} :", end='')
-        for value in values:
-            print(f"{value:^15.3}", end='')
-        print()
