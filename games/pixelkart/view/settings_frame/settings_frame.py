dbe7f490e16bcf97a4f3aca1a1f5a8bb8a516710
NwaSet <samainflorian25@gmail.com>
2026-04-10 14:33:08 +0200

    setting finished

--- games/pixelkart/view/settings_frame/settings_frame.py
+++ games/pixelkart/view/settings_frame/settings_frame.py
@@ -16,8 +16,8 @@ class settings(Frame):
         self.players_frame = Frame(self)
         self.players_frame.pack(pady=20)
 
-        self.player_selection_1 = Frame(self.players_frame, bg="lightblue")
-        self.player_selection_2 = Frame(self.players_frame, bg="lightblue")
+        self.player_selection_1 = Frame(self.players_frame)
+        self.player_selection_2 = Frame(self.players_frame)
 
         settings_player_1 = playerSelection(self.player_selection_1, 1)
         settings_player_2 = playerSelection(self.player_selection_2, 2)
@@ -31,7 +31,7 @@ class settings(Frame):
         # -------------------------
         # FRAME CIRCUIT (EN DESSOUS)
         # -------------------------
-        self.circuit_frame = Frame(self, bg="lightgreen")
+        self.circuit_frame = Frame(self)
         self.circuit_frame.pack(pady=5)
 
         settings_circuit = circuitSelection(self.circuit_frame, self.controler)
